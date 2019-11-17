a29ed0215ee2d4e3144c263ec399fd94d9e620ee
Tiago Giuliatte Pereira <tiagogiuliatte.p@gmail.com>
2019-11-17 18:47:54 -0300

    Input e regra de negócio

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const screen = document.getElementById('screen');
 const context = screen.getContext('2d');
+const currentPlayerId = 'player1';
 
 const game = {
     players: {
@@ -11,21 +12,48 @@ const game = {
     }
 };
 
+document.addEventListener('keydown', handleKeydown);
+
+function handleKeydown(event) {
+    const keyPressed = event.key;
+    const player = game.players[currentPlayerId];
+
+    if (keyPressed === 'ArrowUp' && player.y - 1 >= 0) {
+        player.y--;
+        return;
+    }
+    
+    if (keyPressed === 'ArrowRight' && player.x + 1 < screen.width) {
+        player.x++;
+        return;
+    }
+
+    if (keyPressed === 'ArrowDown' && player.y + 1 < screen.height) {
+        player.y++;
+        return;
+    }
+
+    if (keyPressed === 'ArrowLeft' && player.x - 1 >= 0) {
+        player.x--;
+        return;
+    }
+}
+
 function clearScreen() {
     context.fillStyle = 'white';
-    context.fillRect(0, 0, 10, 10);
+    context.clearRect(0, 0, 10, 10);
 }
 
 (function renderScreen() {
     clearScreen();
 
-    for (playerId in game.players) {
+    for (const playerId in game.players) {
         const player = game.players[playerId];
         context.fillStyle = 'black';
         context.fillRect(player.x, player.y, 1, 1);
     }
 
-    for (fruitId in game.fruits) {
+    for (const fruitId in game.fruits) {
         const fruit = game.fruits[fruitId];
         context.fillStyle = 'green';
         context.fillRect(fruit.x, fruit.y, 1, 1);
